e8447624a69aa43aff10c7dab904247a21086ac6
joobging <152007326+joobging@users.noreply.github.com>
2023-12-01 01:22:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 alert("양양줍겠지 활동 홈페이지 입니다. 구경하시고 설문조사 해주세요!");
-
+console.log("시발");
 const slides = document.querySelector('.slides');
 const slideImg = document.querySelectorAll('.slides !!');
 let currentIdx = 0;
