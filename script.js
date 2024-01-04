aff3693a2cefa7f56227f070fd22f22118406849
4e <152007326+joobging@users.noreply.github.com>
2024-01-04 09:14:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-alert("양양줍겠징 활동 홈페이지 입니다. 구경하시고 설문조사 해주세요!");
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+
+let dx = 2;
+let dy = -2;
+
+const ballRadius = 10;
+
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, 2 * Math.PI);
+  ctx.fillStyle = "#ff9999";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  if (x + dx > canvas.width || x + dx < 0) {
+    dx = -dx;
+  }
+
+  if (y + dy > canvas.height || y + dy < 0) {
+    dy = -dy;
+  }
+  x += dx;
+  y += dy;
+}
+setInterval(draw, 10);
